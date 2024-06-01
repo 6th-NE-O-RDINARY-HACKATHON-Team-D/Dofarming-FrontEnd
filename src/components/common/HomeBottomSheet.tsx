a38f28c985b639e520e948b859c0321637259fc7
kwaksj329 <kwaksj329@gmail.com>
2024-06-02 00:52:25 +0900

    feat: 사진 및 갤러리 버튼 추가

--- src/components/common/HomeBottomSheet.tsx
+++ src/components/common/HomeBottomSheet.tsx
@@ -1,10 +1,13 @@
 import React, {useCallback, useMemo, useRef} from 'react';
 import {BottomSheetBackdrop, BottomSheetModal} from '@gorhom/bottom-sheet';
 import styled from 'styled-components/native';
+import {Text} from 'react-native';
+import PhotoIcon from '../../assets/vectors/photo-icon.svg';
+import GalleryIcon from '../../assets/vectors/gallery-icon.svg';
 
 const HomeBottomSheet = () => {
   const bottomSheetModalRef = useRef<BottomSheetModal>(null);
-  const snapPoints = useMemo(() => ['10%', '30%'], []);
+  const snapPoints = useMemo(() => ['1%', '40%'], []);
 
   const handlePresentModalPress = useCallback(() => {
     bottomSheetModalRef.current?.present();
@@ -32,7 +35,19 @@ const HomeBottomSheet = () => {
         snapPoints={snapPoints}
         onChange={handleSheetChanges}
         backdropComponent={renderBackdrop}>
-        <ChildView />
+        <ChildView>
+          <Text>인증 하기</Text>
+          <BtnView>
+            <ChildBtn>
+              <PhotoIcon />
+              <Text>사진 찍기</Text>
+            </ChildBtn>
+            <ChildBtn>
+              <GalleryIcon />
+              <Text>앨범에서 선택</Text>
+            </ChildBtn>
+          </BtnView>
+        </ChildView>
       </BottomSheetModal>
     </Container>
   );
@@ -44,12 +59,41 @@ const Container = styled.View`
 `;
 
 const ChildView = styled.View`
+  padding-top: 15px;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-bottom: 50px;
   flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   align-items: center;
+`;
+
+const BtnView = styled.View`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `;
 
 const ModalBtn = styled.Button`
   height: 10px;
+`;
+
+const ChildBtn = styled.TouchableOpacity`
+  height: 147px;
+  width: 162px;
+  padding-top: 44px;
+  padding-bottom: 30px;
+  background-color: #f6f7f8;
+  border: 1px;
+  border-color: #ebeef2;
+  border-radius: 12px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 export default HomeBottomSheet;
