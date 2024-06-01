200385adb00f3cdcdf0dd8bd200ce7d02e9e8e6d
alreadynyeong <kjw87130@naver.com>
2024-06-02 06:10:49 +0900

    Feat: 오늘의 미션 연동

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,45 @@
 import {StyleSheet, Animated, View} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Calendar from '../componets/calendar/Calendar';
 import HomeBottomSheet from '../components/common/HomeBottomSheet';
 import {useRef} from 'react';
 import LottieView from 'lottie-react-native';
 import Defarming from '../assets/vectors/defarming-txt.svg';
 
+import axiosInstance from '../api/AxiosInstance';
+
 const Home = () => {
   const confettiRef = useRef<LottieView>(null);
   const opacity = useRef(new Animated.Value(0)).current;
+
+  const [todayMission, setTodayMission] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<any>(null);
+
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, '0');
+  const day = String(today.getDate()).padStart(2, '0');
+
+  const todayString = `${year}-${month}-${day}`;
+
+  useEffect(() => {
+    const fetchMission = async () => {
+      try {
+        const response = await axiosInstance.get(
+          '/api/v1/missions?date=' + todayString,
+        );
+        // console.log('response', response.data.result);
+        setTodayMission(response.data.result);
+        setLoading(false);
+      } catch (error) {
+        console.log('error', error);
+        setError(error);
+        setLoading(false);
+      }
+    };
+    fetchMission();
+  }, []);
 
   function triggerConfetti() {
     confettiRef.current?.play(0);
@@ -41,7 +72,10 @@ const Home = () => {
         resizeMode="cover"
       />
       <Calendar />
-      <HomeBottomSheet triggerConfetti={triggerConfetti} />
+      <HomeBottomSheet
+        triggerConfetti={triggerConfetti}
+        mission={todayMission}
+      />
     </View>
   );
 };
